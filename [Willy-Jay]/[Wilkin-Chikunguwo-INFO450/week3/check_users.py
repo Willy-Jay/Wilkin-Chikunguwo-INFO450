324c2e934da44f547be034aa0832f1d943f9459c
Wilkin Chikunguwo <wchikunguwo@gmail.com>
2020-09-08 10:37:12 -0400

    FINAL Homework Week 3

---
+++ [Willy-Jay]/[Wilkin-Chikunguwo-INFO450/week3/check_users.py
@@ -0,0 +1,24 @@
+def check_users(current_users, new_users):
+
+    pass
+#I renamed the lists from current_us to current_users and new_us to new_users
+current_users = ['chris','haritha', 'sally', 'darnell', 'SUPERMAN']
+
+new_users = ['george', 'ringo', 'superman', 'hannibal']
+#Cool kid way of doing it.
+new_list =[]
+for current_user in current_users:
+    new_list.append(current_user.lower())
+
+current_users = [itm.lower() for itm in current_users]
+
+for new_user in new_users:
+    if  new_user in current_users:
+        print(f'Sorry the username {new_user} is already taken.Please create a new username.')
+    else:
+        print(f'The username {new_user} is availble.')
+        new_users = [itm.lower() for itm in new_users]
+if __name__ == "__main__":
+# I added 'ers' to current_us and new_us.
+# I also moved the current_users and new_users list to lines 5 and 7.
+ check_users(current_users, new_users)
